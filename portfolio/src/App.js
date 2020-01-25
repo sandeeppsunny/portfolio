55160e807a670fb6d524f11f08db6195bee579c6
Sandeep Sunny <sandeepsunny@vmware.com>
2020-01-25 01:25:46 -0700

    On Click

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,15 +5,16 @@ import './App.css';
 import NavigationBarComponent from './components/NavigationBarComponent';
 import JumbotronComponent from './components/JumbotronComponent';
 import AccordionComponent from './components/AccordionComponent';
+import Fade from 'react-reveal/Zoom';
 
 function App() {
   return (
     <div className="App">
       {/*
         <NavigationBarComponent />
+        <AccordionComponent />
       */}
       <JumbotronComponent />
-      <AccordionComponent />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- portfolio/src/components/JumbotronComponent.js
+++ portfolio/src/components/JumbotronComponent.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Jumbotron } from 'react-bootstrap';
 import './JumbotronComponent.css';
+import Zoom from 'react-reveal/Zoom';
 
-function JumbotronComponent() {
-  return (
-    <div className="JumbotronComponent-heading">
-        <Jumbotron fluid className="JumbotronComponent">
-          <Container>
-            <h1 className="test-white">Sandeep Sunny</h1>
-            <p className="text-white">
-              This is my portfolio v1
-            </p>
-          </Container>
-        </Jumbotron>
-    </div>
-  );
+class JumbotronComponent extends Component {
+
+  constructor() {
+    super();
+    this.state = {data: 'Sunny Sandeep'};
+    console.log('Contructor is invoked');
+    this.changeBackground = this.changeBackground.bind(this);
+  }
+
+  changeBackground() {
+      if(this.state.data === 'Fuck you!') {
+        this.setState({data: 'Sandeep Sunny'});
+      } else {
+        this.setState({data: 'Fuck you!'});
+      }
+      console.log("Fuck you!");
+  }
+
+  render() {
+    return (
+      <div className="JumbotronComponent-heading">
+          <Jumbotron fluid className="JumbotronComponent">
+            <Container>
+              <Zoom>
+                <h1 className="test-white" onMouseEnter={this.changeBackground}>{this.state.data}</h1>
+              </Zoom>
+              <p className="text-white">
+                This is my portfolio v1
+              </p>
+            </Container>
+          </Jumbotron>
+      </div>
+    );
+  }
 }
+
 
 export default JumbotronComponent;
