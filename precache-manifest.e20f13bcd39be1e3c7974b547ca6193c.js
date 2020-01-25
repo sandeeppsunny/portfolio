c2793677e50bfff9487a6c190ddd5c27b512c8a4
Sandeep Sunny <sandeepsunny@vmware.com>
2020-01-25 16:30:46 -0700

    Updates

---
+++ precache-manifest.e20f13bcd39be1e3c7974b547ca6193c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731b3a663844d0b72cd85e2beae09d12",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c6a7d4329689127a72e0",
+    "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "08011d6a5dbc1c387b3a",
+    "url": "/portfolio/static/css/main.1b46d5f9.chunk.css"
+  },
+  {
+    "revision": "c6a7d4329689127a72e0",
+    "url": "/portfolio/static/js/2.b15360c2.chunk.js"
+  },
+  {
+    "revision": "73030d41e266afb97ea5369ca6ab6dd2",
+    "url": "/portfolio/static/js/2.b15360c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08011d6a5dbc1c387b3a",
+    "url": "/portfolio/static/js/main.959ec058.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
